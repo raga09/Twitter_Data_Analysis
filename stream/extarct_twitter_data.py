35d6b446c703f7c48d6aa5a87863d45f9f9303d8
raga09 <raga.rachabathuni@gmail.com>
2017-05-13 18:28:23 -0700

    adding python file for extracting t

---
+++ stream/extarct_twitter_data.py
@@ -0,0 +1,41 @@
+import tweepy  # this will give an error if tweepy is not installed properly
+from tweepy import OAuthHandler
+
+# provide your access details below
+access_token = ""
+access_token_secret = ""
+consumer_key = ""
+consumer_secret = ""
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+from tweepy import Stream
+from tweepy.streaming import StreamListener
+
+#public_tweets = api.home_timeline()
+#for tweet in public_tweets:
+#    print(tweet.text)
+
+
+class MyListener(StreamListener):
+    def on_data(self, data):
+        try:
+            with open('D:\\python_data_set\\data_extract.json', 'a') as f:  # change location here
+                f.write(data)
+                return True
+        except BaseException as e:
+            print("Error on_data: %s" % str(e))
+        return True
+
+    def on_error(self, status):
+        print(status)
+        return True
+
+
+twitter_stream = Stream(auth, MyListener())
+
+# change the keyword here
+twitter_stream.filter(track=['#cricket'])
