4695fce577ed811f4a31fd026ed8dea76289c2c4
raga09 <raga.rachabathuni@gmail.com>
2017-05-23 10:24:09 -0700

    adding exception handling

--- stream/extarct_twitter_data.py
+++ stream/extarct_twitter_data.py
@@ -14,15 +14,19 @@ consumer_secret = ""
 geolocator = Nominatim()
 location = geolocator.geocode("hyderabad")
 
-auth = OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+
 
 geocode_ = str(location.latitude) + ',' + str(location.longitude) + ',' + '200mi'
 print(geocode_)
 print(location.address)
 
-api = tweepy.API(auth)
-tweets_list = api.search("%23ipl", rpp=10, lang="en", geocode=geocode_)
+try:
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth)
+    tweets_list = api.search("%23ipl", rpp=10, lang="en", geocode=geocode_)
+except:
+    print("Error: Authentication Failed")
 
 with open('D:\\GitRepos\\Twitter_Data_Analysis\\data\\twitter_data.txt', 'w', encoding='utf-8') as txt_file:
     for status in tweets_list:
